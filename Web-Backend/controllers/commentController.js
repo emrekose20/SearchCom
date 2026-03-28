d95f0b1ab06d1df05a76a225bdeeacdcd46fb05b
Yunus Emre YILDIRIM <byoyun5561@gmail.com>
2026-03-29 00:54:04 +0300

    Update commentController.js

---
+++ Web-Backend/controllers/commentController.js
@@ -0,0 +1,51 @@
+const { Comment } = require("../models");
+
+exports.createComment = async (req, res) => {
+  try {
+    const { userId, establishmentId, content } = req.body;
+
+    if (!userId || !establishmentId || !content) {
+      return res.status(400).json({
+        message: "userId, establishmentId ve content zorunludur."
+      });
+    }
+
+    const comment = await Comment.create({
+      userId,
+      establishmentId,
+      content
+    });
+
+    return res.status(201).json(comment);
+  } catch (error) {
+    return res.status(500).json({
+      message: "Sunucu hatası",
+      error: error.message
+    });
+  }
+};
+
+exports.deleteComment = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const comment = await Comment.findByPk(id);
+
+    if (!comment) {
+      return res.status(404).json({
+        message: "Yorum bulunamadı."
+      });
+    }
+
+    await comment.destroy();
+
+    return res.status(200).json({
+      message: "Yorum başarıyla kaldırıldı."
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Sunucu hatası",
+      error: error.message
+    });
+  }
+};
