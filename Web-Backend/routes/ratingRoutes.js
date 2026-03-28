6da856b297dad0c6043adc67d8e45971959cd782
Yunus Emre YILDIRIM <byoyun5561@gmail.com>
2026-03-28 22:42:12 +0300

    denemee

--- Web-Backend/routes/ratingRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const ratingController = require("../controllers/ratingController");
-
-router.post("/", ratingController.createRating);
-router.get("/establishment/:id", ratingController.getRatingsByEstablishment);
-router.put("/:id", ratingController.updateRating);
-
-module.exports = router;
