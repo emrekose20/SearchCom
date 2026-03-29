97ca9b96c0a2dadadaf61c971867e6c3ccbb5a43
emrekose20 <abdulahkose017@gmail.com>
2026-03-29 03:03:28 +0300

    Update establishmentRoutes.js

--- Web-Backend/routes/establishmentRoutes.js
+++ Web-Backend/routes/establishmentRoutes.js
@@ -1,16 +1,10 @@
 const express = require("express");
-
 const router = express.Router();
-
 const establishmentController = require("../controllers/establishmentController");
  
 router.post("/", establishmentController.createEstablishment);
-
 router.get("/", establishmentController.getAllEstablishments);
-
 router.put("/:id", establishmentController.updateEstablishment);
-
 router.delete("/:id", establishmentController.deleteEstablishment);
  
 module.exports = router;
- 
