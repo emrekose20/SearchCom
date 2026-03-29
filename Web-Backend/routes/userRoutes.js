bff8fedc0242038f335090b90a6d553876e2a21a
emrekose20 <abdulahkose017@gmail.com>
2026-03-29 03:01:06 +0300

    Update userRoutes.js

--- Web-Backend/routes/userRoutes.js
+++ Web-Backend/routes/userRoutes.js
@@ -1,10 +1,16 @@
 const express = require("express");
+
 const router = express.Router();
+
 const userController = require("../controllers/userController");
  
 router.post("/register", userController.registerUser);
+
 router.get("/:id", userController.getUserById);
+
 router.put("/:id", userController.updateUser);
+
 router.delete("/:id", userController.deleteUser);
  
 module.exports = router;
+ 
